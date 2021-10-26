27ea52ac7951cfa0cd73d3c19929387c81fe543f
Felipe Sanches <sanchesmeirelles@gmail.com>
2021-10-26 14:25:05 -0300

    2 e 3 fuctions ok

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ const result = fetch(url)
 .then((data) => data);
 return result
 };
+// retorna um objeto com o name img e level do digimon
+const getDigimonByName = (digimon) => {
+  const url =`https://digimon-api.vercel.app/api/digimon/name/${digimon}`;
+  const result = fetch(url)
+    .then((response) => response.json())
+    .then((data) => data[0]);
+  return result;
+};
+// função que verifica se existe ou não o digimon do input retorna true ou false
+const verifyDigimon = async () => {
+  const list = await getAllDigimons();
+  const nameList = list.map((digimon) => digimon.name);// retorna um array com o nome de todos os digimons
+  const getInputValue = document.querySelector('#digiInput').value;
+  const result = nameList.some((digiName) => digiName.toLowerCase() === getInputValue.toLowerCase());
+  return result;
+};
